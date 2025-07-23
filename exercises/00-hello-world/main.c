dd10d8f6ff007301248cf9aa665a55efc0ec49cd
Michael Hinton <michael.hinton@intel.com>
2025-07-23 00:29:22 -0600

    Make hello world code initially fail to compile

--- exercises/00-hello-world/main.c
+++ exercises/00-hello-world/main.c
@@ -1,11 +1,13 @@
 ////////////////////////////////////////////////////////
 // Task:
+// * Fix compilation error
 // * Print out "Hello, world!"
 // * Add a newline character at the end
 // * Return 0 when finished with above
 ////////////////////////////////////////////////////////
 #include <stdio.h>
 int WinMain() {
+    delete me bad line
     printf("");
     return 1;
 }
