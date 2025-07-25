1b0bdaa863c523511541e305cf399a7fdd04614c
Michael Hinton <github@hinton.e4ward.com>
2025-07-25 09:44:19 -0600

    Update clean.sh to be able to delete solution files

--- clean.sh
+++ clean.sh
@@ -2,5 +2,33 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+
+DELETE_SOLUTIONS="false"
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --solution|-s)
+            DELETE_SOLUTIONS="true"
+            shift
+            ;;
+        --help|-h)
+            echo "Usage: $0 [--solution|-s] [--help|-h]"
+            echo "  --solution, -s   Delete all solution files, to avoid temptation."
+            echo "  --help, -h       Show this help message"
+            exit 0
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+
 echo "Deleting all .bin files"
 find "$SCRIPT_DIR/exercises" -name "*.bin" -type f -delete
+
+if [ "$DELETE_SOLUTIONS" = "true" ]; then
+    echo "Deleting all solution files"
+    find "$SCRIPT_DIR/exercises" -name "solution.c" -type f -delete
+else
+    echo "Skipping deletion of solution files"
+fi
